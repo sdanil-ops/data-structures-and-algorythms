2a16ac521df4af9121a69efaa86b63ceb2b1fc2b
Danil Smirnov <77897780+sdanil-ops@users.noreply.github.com>
2021-03-25 11:10:44 +0300

    feat: add stack realisation

---
+++ data-structures/stack.py
@@ -0,0 +1,28 @@
+#  -----------------------------------------------------------
+#
+#  Python stack data structure realisation
+#
+#  Copyright (c) 2021. Danil Smirnov
+#  Released under GNU Public License (GPL)
+#  email zabanen.nu@ya.ru
+#  -----------------------------------------------------------
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
