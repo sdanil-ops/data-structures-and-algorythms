3ad17760001b2842c5a570148fba59bb6493c885
sdanil-ops <dsmirnov@psystems.biz>
2021-04-09 11:20:42 +0300

    feat: add get_max and get_min
    add min_items and max_items arrays. Contains max\min item for iteration

--- data-structures/stack.py
+++ data-structures/stack.py
@@ -11,18 +11,40 @@
 class Stack:
     def __init__(self):
         self.items = []
+        self.min_items = []
+        self.max_items = []
 
     def is_empty(self):
         return self.items == []
 
     def push(self, item):
-        self.items.append(item)
+        if self.top():
+            if item < self.top():
+                self.min_items.append(item)
+            if item > self.top():
+                self.max_items.append(item)
+            self.items.append(item)
+        else:
+            self.items.append(item)
 
     def pop(self):
         return self.items.pop()
 
-    def peek(self):
-        return self.items[len(self.items) - 1]
-
     def size(self):
         return len(self.items)
+
+    def top(self):
+        if len(self.items) == 0:
+            return None
+        return self.items[len(self.items) - 1]
+
+    def get_min(self):
+        if len(self.min_items) == 0:
+            return None
+        return self.min_items[len(self.min_items) - 1]
+
+    def get_max(self):
+        if len(self.max_items) == 0:
+            return None
+        return self.max_items[len(self.max_items) - 1]
+
